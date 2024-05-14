11e5bb8539e14ef98c077371c34fbc428e541d41
sahalasyari <114453017+sahalasyari@users.noreply.github.com>
2024-05-15 06:18:07 +0700

    Add files via upload

---
+++ ContainString.java
@@ -0,0 +1,15 @@
+public class ContainString {
+    public static void main(String[] args) {
+        String name = "Welcome to Melukis";
+        System.out.println( "Hasilnya : " +name.contains("Melukis"));
+        System.out.println( "Hasilnya : " +name.contains("to"));
+        System.out.println( "Hasilnya : " +name.contains("Hello"));
+
+
+    }
+}
+
+//Output:
+//Hasilnya : true
+//Hasilnya : true
+//Hasilnya : false
